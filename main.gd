a7a31fb931b18a77b84008fa819f98e1e5bfdfda
Zippybyte <62920339+Zippybyte@users.noreply.github.com>
2026-09-22 13:12:12 -0700

    Replace magic numbers from pipe spawn position

--- main.gd
+++ main.gd
@@ -1,12 +1,19 @@
 extends Node
 
 @export var pipe_scene: PackedScene
-var pipe_padding: int = 100
+@export var pipe_range: int = 200
+
+const PIPE_PADDING: int = 100 # Sets how far the pipe spawns off screen
+
+var pipe_spawn_location : Vector2
 
 var score : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Pipe spawns off screen to the right with PIPE_PADDING distance from right edge of screen.
+	# Pipe y value is set to the middle point of the background.
+	pipe_spawn_location = Vector2(get_window().size.x + PIPE_PADDING, $Background.texture.get_height() / 2)
 	get_tree().paused = true
 
 func start_game():
@@ -20,8 +27,10 @@ func generate_pipe():
 	var pipe = pipe_scene.instantiate()
 	
 	# Set location of the pipe
-	pipe.position.x = get_window().size.x + 100
-	pipe.position.y = randi_range(100, 600)
+	pipe.position = pipe_spawn_location
+	
+	# Add an offset from the center of the background
+	pipe.position.y += randi_range(-pipe_range, pipe_range)
 	
 	# Attach signal to increase score when bird goes between a pipe
 	pipe.scored.connect(scored)
